04459fad8f4ea5528d2885c9f50939ea3a1b8b15
jisung <jisung@The-Air.local>
2020-10-30 04:54:17 +0900

    Add function login required

--- photo/urls.py
+++ photo/urls.py
@@ -8,7 +8,7 @@ app_name = 'photo'
 urlpatterns = [
     path('', photo_list, name= 'photo_list'),
     path('upload/', PhotoUploadView.as_view(), name = 'photo_upload'),
-    path('detail/<int:pk>/', DetailView.as_view(model= Photo, template_name='photo/detail.html'), name = 'photo_detail'),
+    path('detail/<int:pk>/', PhotoDetailView.as_view(), name = 'photo_detail'),
     path('delete/<int:pk>/', PhotoDeleteView.as_view(), name = 'photo_delete'),
     path('update/<int:pk>/', PhotoUpdateView.as_view(), name = 'photo_update'),
 ]
